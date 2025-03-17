e0a3c44e2f1fa284a0393f29107f45d3a5a4d397
Daniel Perez <dspr124@gmail.com>
2025-03-17 12:49:04 -0500

    Implementación de Riverpod para la gestión del estado de productos.

---
+++ lib/providers/productos_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../services/api_service.dart';
+
+final apiServiceProvider = Provider((ref) => ApiService());
+
+//Maneja la lista de productos
+final listaProductosProvider = StateNotifierProvider<ProductosNotifier, List<dynamic>>((ref) {
+  return ProductosNotifier(ref.read(apiServiceProvider));
+});
+
+class ProductosNotifier extends StateNotifier<List<dynamic>> {
+  final ApiService _apiService;
+  int _inicio = 0;
+  final int _limite = 10;
+  bool _cargando = false;
+
+  ProductosNotifier(this._apiService) : super([]);
+
+  //Carga mas productos con el scroll
+  Future<void> obtenerMasProductoss() async {
+    if (_cargando) return;
+    _cargando = true;
+
+    final nuevosProductos = await _apiService.obtenerProductos(_inicio, _limite);
+    state = [...state, ...nuevosProductos];
+
+    _inicio += _limite;
+    _cargando = false;
+  }
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,13 +3,13 @@ import 'package:dio/dio.dart';
 class ApiService {
   final Dio _dio = Dio();
 
-  Future<List<dynamic>> fetchProducts(int skip, int limit) async {
+  Future<List<dynamic>> obtenerProductos(int inicio, int limite) async {
     try {
-      final response = await _dio.get(
+      final respuesta = await _dio.get(
         'https://dummyjson.com/products',
-        queryParameters: {'limit': limit, 'skip': skip},
+        queryParameters: {'limit': limite, 'skip': inicio},
       );
-      return response.data['products'];
+      return respuesta.data['products'];
     } catch (e) {
       throw Exception('Error al obtener la lista de productos.');
     }
